ba2bed4d34e30aea6880d4ee0d9cd9dd3cd28cad
nefise <nefise.magalie@gmail.com>
2018-02-07 17:04:49 +0100

    setting up the entity Post.php

---
+++ lib/vendors/Entity/post.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Entitys;
+
+/**
+ * Une classe representent un Post.
+ */
+
+class Post {
+
+	 private $id, $titre, $auteur, $chapo, $contenu, $dateCreation, $dateModification;
+
+
+	//GETTERS //
+	
+	public function  getid()
+	{
+		return $this->id;
+	}
+	public function getAuteur()
+	{
+		return $this->auteur;
+	}
+	public function getTitre()
+	{
+		return $this->titre;
+	}
+	public function getChapo()
+	{
+		return $this->chapo;
+	}
+	public function getContenu()
+	{
+		return $this->contenu;
+	}
+	public function getDateModification()
+	{
+		return $this->dateModification;
+	}
+	public function getDateCreation()
+	{
+		return $this->dateCreation;
+	}
+
+	// SETTERS //
+	
+	public function setId($id)
+	{
+			$this->id = (string) $id;		
+	}
+
+	public function setTitre($titre)
+	{		
+			$this->titre = (string) $titre;	
+	}
+
+	public function setChapo($chapo)
+	{
+			$this->chapo = (string) $chapo;	
+	}
+
+	public function setContenu($contenu)
+	{	
+			$this->contenu = (string) $contenu;
+	}
+
+	public function setAuteur($auteur)
+	{
+			$this->auteur = (string) $auteur;
+	}
+
+	public function setDateModification($dateModification)
+	{
+		$this->dateModification = $dateModification;
+	}
+
+	public function setDateCreation($dateCreation)
+	{
+		$this->dateCreation = $dateCreation;
+	}
+
+}
